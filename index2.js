29ee7bcc425733e8efd4844e3840595e0d91529a
Manas Gupta <117298514+greyhound0@users.noreply.github.com>
2022-12-15 17:57:04 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,12 +1,32 @@
-window.addEventListener('load', () =>{
+window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "rgb(123, 49, 240)",
+        "rgb(223, 245, 27)",
+        "rgb(49, 240, 103)",
+        "rgb(240, 49, 100)",
+        "rgb(49, 186, 240)",
+        "rgb(247, 70, 16)"
+    ]
 
-
+console.log(sounds[0]);
     pads.forEach((pad, index) => {
-        pads.addEventListener('click', function(){
+        pad.addEventListener("click" , function() {
+            sounds[index].currentTime = 0;
             sounds[index].play();
+            createBubbles(index);
         })
     })
+    const createBubbles = (index) => {
+        const bubble = document.createElement('div');
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        })
+    }
 
 })
